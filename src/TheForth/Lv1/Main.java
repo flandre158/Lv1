eb45155a64218e89ecbffe0142d04fc8686b15f8
flandre158 <893037639@qq.com>
2018-11-28 21:20:56 +0800

    Lv1

---
+++ src/TheForth/Lv1/Main.java
@@ -0,0 +1,31 @@
+package TheForth.Lv1;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = JDBCUtil.getConnection();
+        DBTool myDb = new DBTool(conn);
+        try {
+            myDb.createTable();
+            myDb.insert(1, "小白", "white");
+            myDb.insert(2, "小黑", "black");
+            myDb.insert(3, "大黄", "yellow");
+            myDb.insert(4, "小黄", "yellow");
+            myDb.insert(5, "老白", "white");
+            myDb.insert(6, "愣头青", "red");
+            System.out.println("修改前查找数据：");
+            myDb.select(2, "白", "yellow");
+            myDb.update(1, "就是她吗？", "scarlet");
+            myDb.update(2, "威严尽失", "scarlet");
+            myDb.delete(3);
+            System.out.println("修改后数据：");
+            myDb.select(2, "白", "yellow");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.close(myDb.rs, myDb.stmt, myDb.pstmt, myDb.conn);
+        }
+    }
+}
