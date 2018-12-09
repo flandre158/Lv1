e5581e605cddac24b86d23335fb9179e89e0543b
flandre158 <893037639@qq.com>
2018-12-09 21:59:29 +0800

    push

--- src/TheFifth/HTMLHelper.java
+++
@@ -1,4 +0,0 @@
-package TheFifth;
-
-public class HTMLHelper {
-}
---
+++ src/TheFifth/JsonTool.java
@@ -0,0 +1,14 @@
+package TheFifth;
+
+import net.sf.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class JsonTool {
+    public JsonTool (String json) {
+        List<HashMap<String,Object>> dataList;
+        dataList = new ArrayList<>();
+    }
+}
